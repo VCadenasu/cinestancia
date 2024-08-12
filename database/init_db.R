03edeecbbe0ae7661d9b3cef74c67b42e7dd7b0c
huion0905 <valery.armas@upch.pe>
2024-08-12 18:48:50 -0500

    update

--- database/init_db.R
+++ database/init_db.R
@@ -1,11 +1,24 @@
+# database/init_db.R
 library(RSQLite)
 
+# Conexión a la base de datos SQLite
 db <- dbConnect(SQLite(), dbname = "cinestancia.sqlite")
 
+# Crear tabla Users
+dbExecute(db, "
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE
+  )
+")
+
+# Crear tabla Movies
 dbExecute(db, "
   CREATE TABLE IF NOT EXISTS movies (
-    id INTEGER PRIMARY KEY,
-    title TEXT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
     year INTEGER,
     genre TEXT,
     platform TEXT,
@@ -14,13 +27,18 @@ dbExecute(db, "
   )
 ")
 
+# Crear tabla Reviews
 dbExecute(db, "
-  CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY,
-    username TEXT,
-    password TEXT,
-    email TEXT
+  CREATE TABLE IF NOT EXISTS reviews (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    movie_id INTEGER,
+    review TEXT,
+    rating INTEGER,
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(movie_id) REFERENCES movies(id)
   )
 ")
 
+# Cerrar la conexión a la base de datos
 dbDisconnect(db)
